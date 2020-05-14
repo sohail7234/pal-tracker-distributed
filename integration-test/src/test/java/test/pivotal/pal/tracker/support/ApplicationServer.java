cd2686a66a0aef418b2ebeb74b71105d33f6461b
Sohail Shaik <sohailshaik6786@gmail.com>
2020-05-14 13:38:39 +0530

    Update ApplicationServer.java

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -54,7 +54,7 @@ public class ApplicationServer {
 
     private static void waitUntilServerIsUp(String port) throws InterruptedException {
         HttpClient httpClient = new HttpClient();
-        int timeout = 1200;
+        int timeout = 120;
         Instant start = Instant.now();
         boolean isUp = false;
 
